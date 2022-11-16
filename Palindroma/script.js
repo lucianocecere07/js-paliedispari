962e796e0a2827d44d555a1f14f8291522f2ca8e
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-16 14:29:07 +0100

    parola chiesta all'utente

---
+++ Palindroma/script.js
@@ -0,0 +1,14 @@
+//chiedere all'utente di inserire una parola
+//creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata
+
+//parola chiesta all'utente
+let parolaUtente = prompt("Inserisci una parola palindroma");
+let parolaUtenteMinuscolo = parolaUtente.toLowerCase
+
+
+
+
+
+//---------Funzioni--------------
+
+//-------------------------------
