6bd0d4ff6d0e076fcc915c1307e1d2b052ce8506
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-16 14:53:20 +0100

    funzione somma

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -10,9 +10,25 @@ let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
 //generare un numero random da 1 a 5
 let numeroRandom = Math.floor((Math.random() * 5) + 1);
 
+sommaNumeri(numeroUtente, numeroRandom);
+
 
 
 //----------------funzioni--------------
 
+function sommaNumeri(n1, n2){
+    
+    if(numeroUtente < 1 || numeroUtente > 5 || isNaN(numeroUtente)){
+        console.log("Inserisci un numero consentito");
+    } else {
+    let somma = n1 + n2;
+
+    console.log("numero utente: " + numeroUtente);
+    console.log("numero random: " + numeroRandom);
+    console.log("somma: " + somma);
+
+    return somma;
+    }
+}
 
 //-------------------------------------
