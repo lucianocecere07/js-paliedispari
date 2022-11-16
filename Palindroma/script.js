6f016d89c0ed688f3985e380362297063d96d801
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-16 16:35:17 +0100

    scritta funzione per parola palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,22 +1,35 @@
 //chiedere all'utente di inserire una parola
 //creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata
 
+
+
+//---------------Esercizio---------------------------
+
 //parola chiesta all'utente
 let parolaUtente = prompt("Inserisci una parola palindroma");
 let parolaUtenteMinuscolo = parolaUtente.toLowerCase();
 console.log("la parola scelta è: " + parolaUtenteMinuscolo);
 
-let parolaAlContrario = parolaUtenteMinuscolo.split("").reverse("").join("");
-console.log("la parola al contrario è: " + parolaAlContrario);
+palindromo();
+
+//---------------------------------------------------
 
 
 
+//-----------------Funzioni------------------------
 
-//---------Funzioni--------------
+function palindromo() {
 
-function palindromo(){
+    //parola al contrario
+    let parolaAlContrario = parolaUtenteMinuscolo.split("").reverse("").join("");
+    console.log("la parola al contrario è: " + parolaAlContrario);
+
+    if (parolaUtenteMinuscolo == parolaAlContrario) {
+        console.log("La parola " + parolaUtenteMinuscolo + " è palindroma")
+    } else {
+        console.log("La parola " + parolaUtenteMinuscolo + " non è palindroma")
+    }
 
 }
 
-
-//-------------------------------
+//------------------------------------------
